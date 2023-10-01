74d91d6cbd61845a52603c6f007107f44c1859f9
JimmyCauja <jimmycauja@gmail.com>
2023-10-01 09:07:05 -0500

    Se creo el codigo y la pelota rebota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let posX, posY, diam, rad;
+let esp = 34;
+let margen = 50;
+let piso;
+let velY = 1;
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+createCanvas(windowWidth, windowHeight);
+posX = windowWidth / 2;
+posY = 100;
+rectMode(CENTER);
+esp = 34;
+piso = windowHeight - margen - esp / 2;
+diam = 50;
+rad = diam / 2;
 }
 
+
 function draw() {
-  circle(mouseX, mouseY, 20);
+background(0, 200, 150, 70);
+noStroke();
+
+
+posY += velY;
+
+
+if (posY > piso - rad) {
+velY += -1;
+}
+fill(255);
+circle(posX, posY, 50);
+
+
+fill(255, 0, 0);
+rect(windowWidth *0.5, windowHeight - margen, windowWidth * 0.8, 20);
 }
