d09fb82fab2535946c0b199d236b8807d12b85a3
JimmyCauja <jimmycauja@gmail.com>
2023-10-02 15:12:09 -0500

    Se modifico el código y la pelota ya rebota

--- sketch.js
+++ sketch.js
@@ -2,36 +2,51 @@ let posX, posY, diam, rad;
 let esp = 34;
 let margen = 50;
 let piso;
-let velY = 1;
+let acel;
 
 
 function setup() {
 createCanvas(windowWidth, windowHeight);
 posX = windowWidth / 2;
-posY = 100;
+posY = windowHeight * 0.2;
 rectMode(CENTER);
-esp = 34;
-piso = windowHeight - margen - esp / 2;
-diam = 50;
+diam = random(10, 50);
 rad = diam / 2;
+esp = 50;
+margen = 40;
+piso = windowHeight - margen - esp / 2;
+vel = 0;
+acel = 0.98;
+    // frameRate(3);
 }
 
 
 function draw() {
-background(0, 200, 150, 70);
-noStroke();
 
+    background(255, 150, 70);
 
-posY += velY;
+    actualizar();
 
-
-if (posY > piso - rad) {
-velY += -1;
+    noStroke();
+    fill(255, 100, 0);
+    circle(posX, posY, 20);
+    fill(100, 50, 0);
+    rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
 }
-fill(255);
-circle(posX, posY, 50);
 
-
-fill(255, 0, 0);
-rect(windowWidth *0.5, windowHeight - margen, windowWidth * 0.8, 20);
+function actualizar() {
+    vel += acel;
+    posY += vel;
+    
+    if (posY >= piso - rad) {
+        print('ya');
+        vel *= -1;
+        posY += vel;
+    }
 }
+
+
+
+
+
+
