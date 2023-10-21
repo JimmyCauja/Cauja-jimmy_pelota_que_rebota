405056c8960c9987b579fc469d89811d1f5a9d70
JimmyCauja <jimmycauja@gmail.com>
2023-10-20 19:51:47 -0500

    Se modifico el tamaño y la crómatica

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ posY = windowHeight * 0.2;
 rectMode(CENTER);
 diam = random(10, 50);
 rad = diam / 2;
-esp = 50;
-margen = 40;
+esp = 58;
+margen = 46;
 piso = windowHeight - margen - esp / 2;
 vel = 0;
 acel = 0.98;
@@ -23,13 +23,11 @@ acel = 0.98;
 
 function draw() {
 
-    background(255, 150, 70);
-
+    background(230, 130, 50);
     actualizar();
-
     noStroke();
-    fill(255, 100, 0);
-    circle(posX, posY, 20);
+    fill(200, 50, 20);
+    circle(posX, posY, 50);
     fill(100, 50, 0);
     rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
 }
